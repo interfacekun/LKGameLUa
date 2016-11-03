6f7c307608ffecfb2649c6ff8217c3ae07ca3fb5
interfacekun <627795061@qq.com>
2016-11-03 17:01:20 +0800

    add charToNumber function

--- lib1.lua
+++ lib1.lua
@@ -5,3 +5,13 @@ end
 function twice(x)
 	return 2*x
 end
+
+function charToNumber()
+	charInput = io.read()
+	n = tonumber(charInput)
+	if n == nil then
+		error(charInput .. "is not a valid number")
+	else
+		print(n*2)
+	end
+end
