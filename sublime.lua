a3101be995b567a2e1be67d7d33202c9c2429afe
interfacekun <627795061@qq.com>
2016-11-22 17:20:38 +0800

    dd
    dd

--- sublime.lua
+++ sublime.lua
@@ -371,15 +371,23 @@
 -- 	print(t)
 -- end
 
-t = {["x"]="a",["y"]="b"}
-tm = {
-	__index = function ( _,k )
-		for i in pairs(_) do
-			print(type(i),i)
-		end
-		print(k)
-	end
-}
-setmetatable(t, tm)
+-- t = {["x"]="a",["y"]="b"}
+-- tm = {
+-- 	__index = function ( _,k )
+-- 		for i in pairs(_) do
+-- 			print(type(i),i)
+-- 		end
+-- 		print(k)
+-- 	end
+-- }
+-- setmetatable(t, tm)
 
-print(t.z)
+-- print(t.z)
+
+function f()
+	print("aaaaa")
+	return "123"
+end
+
+p = assert(f);
+--print(p());
